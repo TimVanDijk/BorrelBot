86e3351e79c7f3735f85ad3751d88336e3bc3753
Tim van Dijk <tim.vandijk96@gmail.com>
2016-03-08 23:01:02 +0100

    Now actually allows the user to order a drink!

--- www/html/order.php
+++ www/html/order.php
@@ -141,6 +141,74 @@
 		</ul>	
 		
 		<div id="three-columns" class="grid-container" style="display:block;">
+			<?php
+				function ingredientToSlot($ingr) {
+					$available = mysql_fetch_array(mysql_query("SELECT * FROM available"));
+					if ($ingr == $available['slot1'])
+						return 1;
+					else if ($ingr == $available['slot2'])
+						return 2;
+					else if ($ingr == $available['slot3'])
+						return 3;
+					else if ($ingr == $available['slot4'])
+						return 4; 
+				}
+
+
+				if (isset($_GET['order']) && !isset($_POST['order'])){
+					echo "Confirm order: " . $_GET['order'];
+					echo '	<form action="order.php" method="POST">
+								<input type="hidden" name="order" value="'. $_GET['order'] .'">
+								<input type="submit" value="Order!">
+							</form>';
+				}
+				else if (isset($_POST['order'])){
+					$host = "localhost";
+					$port = 12345;
+
+					$dbhost = 'localhost';
+					$dbuser = 'root';
+					$dbpass = 'pass123';
+
+					$conn = mysql_connect($dbhost, $dbuser, $dbpass)
+					or die('Error connecting to mysql');
+
+					$dbname = 'borrelbot';
+					mysql_select_db($dbname);
+
+					$f = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+					socket_set_option($f, SOL_SOCKET, SO_SNDTIMEO, array('sec' => 1, 'usec' => 500000));
+					$s = socket_connect($f, $host, $port);
+
+
+					$available = mysql_fetch_array(mysql_query("SELECT * FROM available"));
+					$msg = "";
+
+					$amounts = array(0,0,0,0);
+					$slots = array('slot_1', 'slot_2', 'slot_3', 'slot_4');
+					$ingr_columns = array('ingr_1', 'ingr_2', 'ingr_3', 'ingr_4');
+					$amnt_columns = array('amnt_1', 'amnt_2', 'amnt_3', 'amnt_4');
+
+					$query = "SELECT * FROM cocktails WHERE name ='".$_POST['order']. "'";
+					while ($cocktail = mysql_fetch_array(mysql_query($query))) {
+						for ($i = 0; $i < 4; $i++) {
+							for ($j = 0; $j < 4; $j++) {
+								if ($available[$slots[$i]] == $cocktail[$ingr_columns[$j]]) {
+									$amounts[$i] = $cocktail[$amnt_columns[$j]];
+								}
+							}
+						}
+						break;
+					}
+					$msg = $amounts[0]."-".$amounts[1]."-".$amounts[2]."-".$amounts[3]; 
+					$len = strlen($msg);
+
+					socket_sendto($f, $msg, $len, 0, $host, $port);
+
+					socket_close($f);
+					echo "Order placed!";
+				}_
+			?>
 			<h3>Currently available</h3>
 			<ul class="rig columns-3">
 			<?php
@@ -183,7 +251,7 @@
 
 			foreach ($makeable as $cocktail) {
 				echo'<li>
-					<img src="' . $cocktail['picture'] . '" />
+					<a href="order.php?order=' . $cocktail['name'] . '"><img src="' . $cocktail['picture'] . '" /></a>
 					<h3>'. $cocktail['name'] . '</h3>
 					<p>';
 					if ($cocktail['ingr_1'] != null) {

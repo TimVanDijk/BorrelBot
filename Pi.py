518c9c4e9b3386d777c2612eae17322b19b5684b
Tim van Dijk <tim.vandijk96@gmail.com>
2016-03-14 15:12:10 +0100

    Adjusted to new protocol

--- Pi.py
+++ Pi.py
@@ -1,67 +1,22 @@
 import serial
-# import random
 import time
 import socket
+import sys
 
-#ser = serial.Serial('/dev/ttyUSB2', 9600)
-def flushAll():
-	ser.flushInput()
-	ser.flushOutput()
+try: 
+	ser = serial.Serial('/dev/ttyUSB0', 9600)
+except serial.SerialException:
+	try:
+		ser = serial.Serial('/dev/ttyUSB1', 9600)
+	except serial.SerialException:
+		try:
+			ser = serial.Serial('/dev/ttyUSB2', 9600)
+		except serial.SerialException:
+			print("Cannot find arduino :(")
+			print("Exiting...")
+			sys.exit(1)
 
-# Next time is for pump n
-# Protocol in-depth in *.ino
-def sendPump(n):
-	print ("Send: p"+str(n))
-	ser.write (("p"+str(n)+"#").encode())
-	tmp = ser.readline()
-	flushAll()
-	print "Recv: " + str(tmp[0])
-	if tmp[0] == str(n):
-		ser.write("k#".encode())
-		print "Send: k"
-	else:
-		ser.write("x#".encode())
-		print "Send: x"
-		sendPump(n)
-	flushAll()
-
-# time for previously sent pump is n.
-# Protocol in-depth in *.ino
-def sendMillis(n, s):
-	ser.write (("m"+str(n)+"#").encode()) #TODO < 100
-	print ("Send: m"+str(n))
-	time.sleep(1)
-	tmp = ser.readline()
-	flushAll()
-	print "Recv: " + str(tmp[:len(str(n))])
-	if str(n) == tmp[:len(str(n))]:
-		if (s == True):
-			ser.write ("s".encode())
-			print ("Send: s")
-		else:
-			ser.write ("k#".encode())
-			print ("Send: k")
-		flushAll()
-		time.sleep(0.2)
-		tmp = ser.readline()
-		print "Recv: " + str(tmp)
-	else:
-		ser.write("x#".encode())
-		print "Send: x"
-		sendMillis(n, s)
-	flushAll()
-
-# Start of Program
 time.sleep(5) # Needed to open connection
-# test:
-#sendPump(0);
-#sendMillis(230, False);
-#sendPump(1);
-#sendMillis(99, False);
-#sendPump(2);
-#sendMillis(1, False);
-#sendPump(3);
-#sendMillis(0, True);
 s = socket.socket()
 host = "localhost"
 port = 12345
@@ -76,13 +31,9 @@ while True:
 	if data:
 		print "Received order: " + str(data)
 		amounts = data.split("-")
-		while True:
-			sendPump(0)
-			sendMillis(amounts[0], False)
-			sendPump(1)
-			sendMillis(amounts[1], False)
-			sendPump(2)
-			sendMillis(amounts[2], False)
-			sendPump(3)
-			sendMillis(amounts[3], True)
-	
+		msg = ""
+		for amount in amounts:
+			msg += amount.zfill(4)
+		msg += "#"
+		print("Sending message: " + msg)
+		ser.write(msg.encode())	

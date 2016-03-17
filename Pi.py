7d24c94f20dd3b5a7cb2e3775c0aaacfe413030a
Tim van Dijk <tim.vandijk96@gmail.com>
2016-03-17 20:09:06 +0100

    Added ASCII art

--- Pi.py
+++ Pi.py
@@ -4,6 +4,18 @@ import socket
 import sys
 from threading import Thread
 
+print("""
+888888b.                                    888 888888b.            888    
+888  "88b                                   888 888  "88b           888    
+888  .88P                                   888 888  .88P           888    
+8888888K.   .d88b.  888d888 888d888 .d88b.  888 8888888K.   .d88b.  888888 
+888  "Y88b d88""88b 888P"   888P"  d8P  Y8b 888 888  "Y88b d88""88b 888    
+888    888 888  888 888     888    88888888 888 888    888 888  888 888    
+888   d88P Y88..88P 888     888    Y8b.     888 888   d88P Y88..88P Y88b.  
+8888888P"   "Y88P"  888     888     "Y8888  888 8888888P"   "Y88P"   "Y888
+
+""")
+
 #Used to kill the arduinoHandler thread.
 kill_received = False
 
@@ -20,12 +32,13 @@ except serial.SerialException:
 		try:
 			ser = serial.Serial('/dev/ttyUSB2', 9600)
 		except serial.SerialException:
-			print("Cannot find arduino :(")
-			print("Exiting...")
-			#sys.exit(1)
-print("Arduino found. Waiting for the connection to be established...")			
+			print("[-] - Cannot find arduino.")
+			print("[-] - Exiting...")
+			sys.exit(1)
+print("[+] - Arduino found!")
+print("[*] - Waiting for the connection to be established...")			
 time.sleep(5) #Leave this out and we get weird readings from the serial.
-print("Done!")
+print("[+] - Done!")
 
 #Checks the queue for orders and forwards them to the arduino
 def arduinoHandler():
@@ -37,18 +50,18 @@ def arduinoHandler():
 		if queue != []:
 			msg = queue[0]
 			queue = queue[1:]
-			print("Sending message: " + msg)
+			print("[*] - Sending message: " + msg)
 			if True:
 				ser.write(msg.encode())	
 				#Wait for the arduino to respond. Then proceed to next order.
 				while not kill_received:
 					if ser.readline() != "":
-						print("Drink served!")
+						print("[+] - Drink served!")
 						break
 		else:
 			#Wait 3 seconds before checking again
 			time.sleep(3)
-	print("arduinoHandler thread stops now.")
+	print("[*] - arduinoHandler thread stops now.")
 
 def main():
 	global queue
@@ -79,8 +92,8 @@ def main():
 				queue.append(msg)
 			
 	except KeyboardInterrupt:
-		print("\nReceived KeyboardInterrupt.")
-		print("Telling arduinoHandler thread to stop.")
+		print("\n[*] - Received KeyboardInterrupt.")
+		print("[*] - Telling arduinoHandler thread to stop.")
 		#Tell the arduinoHandler to stop
 		kill_received = True
 		#Close the socket

4d08cfa2ebfb4e24aaf389fd1ff5edf53734eb60
Tim van Dijk <tim.vandijk96@gmail.com>
2016-03-17 19:01:45 +0100

    Documented code and removed debug code

--- Pi.py
+++ Pi.py
@@ -4,6 +4,13 @@ import socket
 import sys
 from threading import Thread
 
+#Used to kill the arduinoHandler thread.
+kill_received = False
+
+#Queue of orders that still need to be forwarded to the arduino.
+queue = []
+
+#Try to find an arduino and then create a connection.
 try: 
 	ser = serial.Serial('/dev/ttyUSB0', 9600)
 except serial.SerialException:
@@ -16,49 +23,47 @@ except serial.SerialException:
 			print("Cannot find arduino :(")
 			print("Exiting...")
 			#sys.exit(1)
-print("Arduino found. Waiting for the connection be to established...")			
-time.sleep(5) # Needed to open connection
+print("Arduino found. Waiting for the connection to be established...")			
+time.sleep(5) #Leave this out and we get weird readings from the serial.
 print("Done!")
 
-kill_received = False
-
-queue = []
-
+#Checks the queue for orders and forwards them to the arduino
 def arduinoHandler():
 	global queue
 	global ser
 	global kill_received
 
 	while not kill_received:
-		print(queue)
 		if queue != []:
 			msg = queue[0]
 			queue = queue[1:]
 			print("Sending message: " + msg)
-			time.sleep(5)
-			if False:
+			if True:
 				ser.write(msg.encode())	
+				#Wait for the arduino to respond. Then proceed to next order.
 				while not kill_received:
-					inp = ser.readline()
-					if inp != "":
-						print(inp)
-						break;
+					if ser.readline() != "":
+						print("Drink served!")
+						break
 		else:
+			#Wait 3 seconds before checking again
 			time.sleep(3)
+	print("arduinoHandler thread stops now.")
 
 def main():
+	global queue
+	global kill_received
+
 	try:
 		t = Thread(target=arduinoHandler)
 		t.start()
 		
+		#Used for communication with the webinterface. 
 		s = socket.socket()
 		host = "localhost"
 		port = 12345
 		s.bind((host, port))
 		s.listen(5)
-
-		global queue
-		global kill_received
 
 		while not kill_received:
 			c, addr = s.accept()
@@ -74,12 +79,13 @@ def main():
 				queue.append(msg)
 			
 	except KeyboardInterrupt:
-		print("\nReceived KeyboardInterrupt")
-		global kill_received
+		print("\nReceived KeyboardInterrupt.")
+		print("Telling arduinoHandler thread to stop.")
+		#Tell the arduinoHandler to stop
 		kill_received = True
+		#Close the socket
 		if s:
 			s.close()
 
 if __name__ == "__main__":
 	main()
-

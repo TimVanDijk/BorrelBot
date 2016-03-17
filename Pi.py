ca4778b6d4a4118dc2c4415518b2cb61c4fe2614
Tim van Dijk <tim.vandijk96@gmail.com>
2016-03-17 12:50:25 +0100

    Multithreaded the program to allow queueing of orders

--- Pi.py
+++ Pi.py
@@ -2,6 +2,8 @@ import serial
 import time
 import socket
 import sys
+from threading import Thread
+
 try: 
 	ser = serial.Serial('/dev/ttyUSB0', 9600)
 except serial.SerialException:
@@ -13,32 +15,71 @@ except serial.SerialException:
 		except serial.SerialException:
 			print("Cannot find arduino :(")
 			print("Exiting...")
-			sys.exit(1)
-
+			#sys.exit(1)
+print("Arduino found. Waiting for the connection be to established...")			
 time.sleep(5) # Needed to open connection
-s = socket.socket()
-host = "localhost"
-port = 12345
-s.bind((host, port))
+print("Done!")
 
-s.listen(5)
+kill_received = False
 
-while True:
-	c, addr = s.accept()
-	data = c.recv(1024)
-	c.close()
-	if data:
-		print "Received order: " + str(data)
-		amounts = data.split("-")
-		msg = ""
-		for amount in amounts:
-			msg += amount.zfill(4)
-		msg += "#"
-		print("Sending message: " + msg)
-		ser.write(msg.encode())	
-		while True:
-			inp = ser.readline()
-			if inp != "":
-				print(inp)
-				break;
-			time.sleep(1)
+queue = []
+
+def arduinoHandler():
+	global queue
+	global ser
+	global kill_received
+
+	while not kill_received:
+		print(queue)
+		if queue != []:
+			msg = queue[0]
+			queue = queue[1:]
+			print("Sending message: " + msg)
+			time.sleep(5)
+			if False:
+				ser.write(msg.encode())	
+				while not kill_received:
+					inp = ser.readline()
+					if inp != "":
+						print(inp)
+						break;
+		else:
+			time.sleep(3)
+
+def main():
+	try:
+		t = Thread(target=arduinoHandler)
+		t.start()
+		
+		s = socket.socket()
+		host = "localhost"
+		port = 12345
+		s.bind((host, port))
+		s.listen(5)
+
+		global queue
+		global kill_received
+
+		while not kill_received:
+			c, addr = s.accept()
+			data = c.recv(1024)
+			c.close()
+			if data:
+				print "Received order: " + str(data)
+				amounts = data.split("-")
+				msg = ""
+				for amount in amounts:
+					msg += amount.zfill(4)
+				msg += "#"
+				queue.append(msg)
+			
+	except KeyboardInterrupt:
+		print("\nReceived KeyboardInterrupt")
+		global kill_received
+		kill_received = True
+		if s:
+			s.close()
+
+if __name__ == "__main__":
+	main()
+
